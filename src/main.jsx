9f98cf513f690c831e1e2ee61ffd4e21f7536643
Roarlor29 <roar.lorentsen@gmail.com>
2026-09-14 18:55:22 +0200

    Aktiver persistent lagring av inputverdier

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './PreciseApp.jsx'
 import './v2.css'
+import './formPersistence.js'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
